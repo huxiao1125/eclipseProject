b68e9845aba5bf281ede7817aaf400ea4b5e4ec5
hx <691623892@11.com>
2019-03-06 13:31:08 +0800

    asd

---
+++ src/com/hx/zdybq/Demo.java
@@ -0,0 +1,17 @@
+/** 
+ * @File Desc:
+ * @author 胡潇
+ * @2019年3月6日 下午1:30:42
+*/
+package com.hx.zdybq;
+
+/** 
+ * 描述:
+ * @author 胡潇
+ * 创建时间 2019年3月6日 下午1:30:42
+ */
+public class Demo {
+public static void main(String[] args) {
+	System.out.println("1221");
+}
+}
